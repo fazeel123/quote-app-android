16e055c75cada599874b1c74662706d773dba0c4
Fazeel Ahmed Khan <fazeelahmedkhan15@gmail.com>
2021-03-22 17:29:36 +0500

    Initial commit

---
+++ app/src/main/java/com/app/thequote/MainActivity.java
@@ -0,0 +1,27 @@
+package com.app.thequote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
